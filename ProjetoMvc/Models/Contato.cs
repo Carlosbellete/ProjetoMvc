92acd41a96e8b7ab81723a6d73596890da2f1b04
Carlos Bellete <dcarlosbellete@gmail.com>
2023-07-06 23:46:41 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoMvc/Models/Contato.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoMvc.Models
+{
+    [Table("Contato")]
+    public class Contato
+    {
+        [Column("Id")]
+        [Display(Name ="Id")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("Telefone")]
+        [Display(Name = "Telefone")]
+        public string Telefone { get; set; }
+
+        [Column("Ativo")]
+        [Display(Name = "Ativo")]
+        public bool Ativo { get; set; }
+    }
+}
